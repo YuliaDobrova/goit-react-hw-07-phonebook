81abe68656018f1a7431c4d17d70924aedb99896
Yulia Dobrova <dobrova2009@gmail.com>
2021-08-09 13:06:07 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="appHeading">Phonebook</h1>
+
+      <h2 className="appHeading">Contacts</h2>
+    </>
+  );
+};
+
+export default App;
